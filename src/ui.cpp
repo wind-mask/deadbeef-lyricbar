d9cd74fd1a122c3f8022607f64d4f33351162867
wind_mask <wind_mask_proton@proton.me>
2024-10-12 19:52:18 +0800

    fix: 🐛 fix -Werror=format-security to use makepkg

--- src/ui.cpp
+++ src/ui.cpp
@@ -262,7 +262,7 @@ extern "C"
 	");
 
   // load css
-  auto data = g_strdup_printf(cssconfig.c_str());
+  auto data = g_strdup_printf("%s", cssconfig.c_str());
 
   RefPtr<Gtk::CssProvider> cssProvider = Gtk::CssProvider::create();
   cssProvider->load_from_data(data);
